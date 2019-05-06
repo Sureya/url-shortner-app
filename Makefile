bd63fb5b214ddf0ddfa502a0e7daeb774269a377
Sureya Sathiamoorthi <sureyasathiamoorthi@Sureyas-MBP.broadband>
2019-05-06 02:27:27 +0100

    V1 working port altered to 9558 via gunicorn binding

---
+++ Makefile
@@ -0,0 +1,9 @@
+push-code:
+	git add .
+	git commit -m "Automated push at : $(date)"
+	git push
+
+push-code:
+	git add .
+	git commit -m "Automated push at : $(date)"
+	git push
--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import (Flask, render_template, redirect, request, url_for, flash, g)
+# in-built
 import logging
-import short_url
 import uuid
 import os
+
+# 3rd party
+from flask import (Flask, render_template, redirect, request, url_for, g)
 import psycopg2
+import short_url
+
 
 app = Flask(__name__)
 logging.getLogger().setLevel(logging.DEBUG)
@@ -12,16 +16,16 @@ short_url.DEFAULT_BLOCK_SIZE = 4
 
 @app.before_request
 def before_request():
-   g.db =psycopg2.connect(user="developer", password="developer", host="127.0.0.1", port="5432",
-                          database="exercises")
+   g.db = psycopg2.connect(user="developer", password="developer", host="127.0.0.1", port="5432",
+                           database="exercises")
 
    g.cursor = g.db.cursor()
-   g.domain = os.environ.get("DOMAIN", "localhost:5000")
+   g.domain = os.environ.get("DOMAIN", "localhost:9558")
    g.protocol = os.environ.get("PROTOCOL", "http")
 
 
 @app.teardown_request
-def teardown_request(exception):
+def teardown_request(execption):
     g.cursor.close()
     g.db.close()
 
@@ -48,7 +52,7 @@ def home_page():
         result_url = f"{g.protocol}://{g.domain}/{encoded_hash}"
         persist_records(_id, url, result_url.replace("'", ""))
         logging.info(f"record updated in database, {result_url}")
-        return redirect(url_for('shortened_url', url=result_url)), 200
+        return redirect(url_for('shortened_url', url=result_url)), 302
 
     else:
         return render_template("index.html"), 200
@@ -60,13 +64,8 @@ def shortened_url():
     return render_template('result.html', url=url), 200
 
 
-#  WIP ******
 @app.route('/<hash>')
 def redirect_url(hash):
     res = get_url_record(short_url.decode_url(hash))
     logging.info(res)
-    return redirect(res)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    return redirect(res, code=302)
